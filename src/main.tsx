02211201808612764d5f44deb93203481a951c99
Mk2717 <mikeezym@gmail.com>
2026-08-06 16:56:18 +0000

    Mount Sprint 1.3 CRM workspace

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import DashboardExperience from './components/DashboardExperience';
+import CustomerCRM from './components/CustomerCRM';
 import './styles.css';
 import './vercel-overrides.css';
 import './auth-v2.css';
+import './crm.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
       <DashboardExperience />
+      <CustomerCRM />
       <App />
     </HashRouter>
   </React.StrictMode>,
