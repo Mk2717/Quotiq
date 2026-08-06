353602733a6ce9b549e257599e8e47001dcd11b4
Mk2717 <mikeezym@gmail.com>
2026-08-06 18:32:25 +0000

    Route estimates to the new quotation studio

--- src/components/RoutedApp.tsx
+++ src/components/RoutedApp.tsx
@@ -1,9 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import App from '../App';
 import { WorkspacePages } from './WorkspacePages';
+import QuoteStudio from './QuoteStudio';
 
 export default function RoutedApp(){
  const location=useLocation();
- const isWorkspaceRoute=location.pathname.startsWith('/customers')||location.pathname.startsWith('/estimates');
- return isWorkspaceRoute?<WorkspacePages/>:<App/>;
+ if(location.pathname.startsWith('/estimates')) return <QuoteStudio/>;
+ if(location.pathname.startsWith('/customers')) return <WorkspacePages/>;
+ return <App/>;
 }
