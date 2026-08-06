e29d3e0b6510dafb0d414621d33b62e34eeb10ec
Mk2717 <mikeezym@gmail.com>
2026-08-06 18:38:31 +0000

    Remove estimate studio routing and reset estimates

--- src/components/RoutedApp.tsx
+++ src/components/RoutedApp.tsx
@@ -1,11 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import App from '../App';
 import { WorkspacePages } from './WorkspacePages';
-import QuoteStudio from './QuoteStudio';
+import EstimateReset from './EstimateReset';
 
 export default function RoutedApp(){
  const location=useLocation();
- if(location.pathname.startsWith('/estimates')) return <QuoteStudio/>;
+ if(location.pathname.startsWith('/estimates')) return <EstimateReset/>;
  if(location.pathname.startsWith('/customers')) return <WorkspacePages/>;
  return <App/>;
 }
