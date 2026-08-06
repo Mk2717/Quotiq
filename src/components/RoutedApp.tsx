28a6e0d18596da6054ea67efa463f1cd25144a02
Mk2717 <mikeezym@gmail.com>
2026-08-06 18:47:14 +0000

    Route estimates to clean Estimates V2 module

--- src/components/RoutedApp.tsx
+++ src/components/RoutedApp.tsx
@@ -1,11 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import App from '../App';
 import { WorkspacePages } from './WorkspacePages';
-import EstimateReset from './EstimateReset';
+import EstimatesV2 from './EstimatesV2';
 
 export default function RoutedApp(){
  const location=useLocation();
- if(location.pathname.startsWith('/estimates')) return <EstimateReset/>;
+ if(location.pathname.startsWith('/estimates')) return <EstimatesV2/>;
  if(location.pathname.startsWith('/customers')) return <WorkspacePages/>;
  return <App/>;
 }
