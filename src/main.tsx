72a3034f91286ef08f389d620e2211697ab8da2b
Mk2717 <mikeezym@gmail.com>
2026-08-06 17:50:32 +0000

    Replace parallel overlays with exclusive routed application pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './lib/bootstrapWorkspace';
-import App from './App';
-import RouteModules from './components/RouteModules';
-import DocumentLauncher from './components/DocumentLauncher';
-import DocumentWorkspace from './components/DocumentWorkspace';
-import EstimatePanelControls from './components/EstimatePanelControls';
+import RoutedApp from './components/RoutedApp';
 import './styles.css';
 import './vercel-overrides.css';
 import './auth-v2.css';
-import './crm.css';
-import './estimate-builder.css';
-import './document-workspace.css';
-import './estimate-panel-controls.css';
-import './module-layout-fix.css';
+import './workspace-pages.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
-      <RouteModules />
-      <DocumentLauncher />
-      <DocumentWorkspace />
-      <EstimatePanelControls />
+      <RoutedApp />
     </HashRouter>
   </React.StrictMode>,
 );
