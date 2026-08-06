42f9792a575246d91aca3823dc7f42e9bfc3d8fc
Mk2717 <mikeezym@gmail.com>
2026-08-06 15:49:39 +0000

    Mount enhanced dashboard experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,15 @@
-import React from 'react';import{createRoot}from'react-dom/client';import{HashRouter}from'react-router-dom';import App from './App';import'./styles.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><HashRouter><App/></HashRouter></React.StrictMode>);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import DashboardExperience from './components/DashboardExperience';
+import './styles.css';
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <DashboardExperience />
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+);
