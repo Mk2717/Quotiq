40fe513ce705ec3225a39d732ab8e3e553e02cf5
Mk2717 <mikeezym@gmail.com>
2026-08-06 17:38:26 +0000

    Load unified module layout fix

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './crm.css';
 import './estimate-builder.css';
 import './document-workspace.css';
 import './estimate-panel-controls.css';
+import './module-layout-fix.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
