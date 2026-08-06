e4df11811c9d04146f7c54e38700c43b2fb26083
Mk2717 <mikeezym@gmail.com>
2026-08-06 17:01:17 +0000

    Mount advanced estimate builder

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,19 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import DashboardExperience from './components/DashboardExperience';
 import CustomerCRM from './components/CustomerCRM';
+import EstimateBuilder from './components/EstimateBuilder';
 import './styles.css';
 import './vercel-overrides.css';
 import './auth-v2.css';
 import './crm.css';
+import './estimate-builder.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
       <DashboardExperience />
       <CustomerCRM />
+      <EstimateBuilder />
       <App />
     </HashRouter>
   </React.StrictMode>,
