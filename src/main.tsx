3b9df199c5b022580c8cb716da1254b270992c3f
Mk2717 <mikeezym@gmail.com>
2026-08-06 16:00:37 +0000

    Load Vercel logo asset overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import DashboardExperience from './components/DashboardExperience';
 import './styles.css';
+import './vercel-overrides.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
